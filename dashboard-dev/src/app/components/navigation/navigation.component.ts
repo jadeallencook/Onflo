a9952b58a8988afa344c05e4b9d06fa0a59bef6c
Jade Allen Cook <jadeallencook@gmail.com>
2017-09-16 01:26:01 -0600

    ⚙️ Completed settings page for dashboard

--- dashboard-dev/src/app/components/navigation/navigation.component.ts
+++ dashboard-dev/src/app/components/navigation/navigation.component.ts
@@ -4,8 +4,7 @@ import { FirebaseAuthService } from '../../services/firebase-auth/firebase-auth.
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css'],
-  providers: [FirebaseAuthService]
+  styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
 
--- dashboard-dev/src/app/pages/settings/settings.component.ts
+++ dashboard-dev/src/app/pages/settings/settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseAuthService } from '../../services/firebase-auth/firebase-auth.service';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-settings',
@@ -7,7 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  business:string = '';
+  phone:string = '';
+  website:string = '';
+  slack:string = '';
+  changesSaved:boolean = false;
+
+  constructor(private FirebaseAuthService: FirebaseAuthService) {
+    firebase.database().ref('users/' + this.FirebaseAuthService.ActiveUser.uid).on('value', (snapshot) => {
+      let user = snapshot.val();
+      if (user.business) this.business = user.business;
+      if (user.phone) this.phone = user.phone;
+      if (user.website) this.website = user.website;
+      if (user.slack) this.slack = user.slack;
+    });
+  }
+
+  saveChanges() {
+    let date = new Date().toString();
+    firebase.database().ref('users/' + this.FirebaseAuthService.ActiveUser.uid).set({
+      updated: date,
+      business: this.business,
+      phone: this.phone,
+      website: this.website,
+      slack: this.slack
+    }).then(() => {
+      this.changesSaved = true;
+      let successMsgOn = setInterval(() => {
+        this.changesSaved = false;
+        clearInterval(successMsgOn);
+      }, 5000);
+    });
+  }
 
   ngOnInit() {
   }

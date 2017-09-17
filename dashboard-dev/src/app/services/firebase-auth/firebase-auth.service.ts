1652dbc8859cf2803d73d3961a74115749dd313e
Jade Allen Cook <jadeallencook@gmail.com>
2017-09-17 10:30:34 -0600

    📱 Made dashboard more mobile friendly and edited content

--- dashboard-dev/src/app/services/firebase-auth/firebase-auth.service.ts
+++ dashboard-dev/src/app/services/firebase-auth/firebase-auth.service.ts
@@ -18,17 +18,23 @@ export class FirebaseAuthService {
   // firebase
 
   createAccount(email: string, password: string) {
-    this.AngularFireAuth.auth.createUserWithEmailAndPassword(email, password).then(() => {
-      this.login(email, password);
-      firebase.auth().onAuthStateChanged((user) => {
-        let date = new Date().toString();
-        firebase.database().ref('users/' + user.uid).set({
-          created: date
+    if (!email && !password) {
+      this.errorMsg = 'Umm, you need both an email and password first before we go anywhere.';
+    } else if (!email || !password) {
+      this.errorMsg = 'So what, you just got half of it done and quit?';
+    } else {
+      this.AngularFireAuth.auth.createUserWithEmailAndPassword(email, password).then(() => {
+        this.login(email, password);
+        firebase.auth().onAuthStateChanged((user) => {
+          let date = new Date().toString();
+          firebase.database().ref('users/' + user.uid).set({
+            created: date
+          });
         });
+      }).catch((error) => {
+        this.errorMsg = error.message;
       });
-    }).catch((error) => {
-      this.errorMsg = error.message;
-    });
+    }
   }
 
   login(email: string, password: string) {
